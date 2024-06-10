0e641ea0534ed4f5f00b952ef01772dee4b17ae0
rajkumar-ambadipelli <rajkumarambadipelli@gmai.com>
2024-06-10 22:18:08 +0530

    Added files

--- learn_git.sh
+++ learn_git.sh
@@ -10,3 +10,7 @@ echo "Releasing version2"
 
 
 echo "Tut for changing the version history of the this file: Version3"
+
+
+# git config --global alias.s "status"
+# the above is used for setting aliases
